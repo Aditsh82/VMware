b3fe9a388cea06cec40e8b02d6d94f38d0232bc8
codedan <54751134+Aditsh82@users.noreply.github.com>
2019-12-16 00:53:15 +0530

    Create UBUNTU_HELP.py

---
+++ UBUNTU_HELP.py
@@ -0,0 +1,7 @@
+UBUNTU HELP FR PANEL LINKE WINDOWS 7
+
+
+https://github.com/home-sweet-gnome/dash-to-panel.git
+
+1. Go to Ubuntu Store
+2. Search for key word "dash to panel" download the extension and install
